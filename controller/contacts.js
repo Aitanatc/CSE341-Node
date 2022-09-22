aff64c26b6de75650ff8488af2ed43e823daa24e
Aitanantc <aitanantc@gmail.com>
2022-09-22 17:47:18 -0500

    fixed GET AND POST

--- controller/contacts.js
+++ controller/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../database/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db().collection('Contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -14,7 +14,7 @@ const getSingle = async (req, res, next) => {
   const result = await mongodb
     .getDb()
     .db()
-    .collection('contacts')
+    .collection('Contacts')
     .find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
@@ -22,4 +22,61 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.FirstName,
+    lastName: req.body.LastName,
+    email: req.body.Email,
+    favoriteColor: req.body.FavoriteColor,
+    birthday: req.body.Birthday
+  };
+  const response = await mongodb.getDb().db().collection('Contacts').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || "Sorry, something happened. Try again");
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('Contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Sorry, and error occurred.');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('Contacts').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Sorry, and error occurred.');
+  }
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
+};
+
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,20 @@ const router = express.Router();
 
 const contactsController = require('../controller/contacts');
 
+//find and get all
 router.get('/', contactsController.getAll);
 
+//find with the id
 router.get('/:id', contactsController.getSingle);
+
+//create a new contact with POST
+router.post('/', contactsController.createContact);
+
+//create a PUT route to update a contact..
+router.put('/:id', contactsController.updateContact);
+
+//delete a contact
+router.delete('/:id', contactsController.deleteContact);
+
 
 module.exports = router;
